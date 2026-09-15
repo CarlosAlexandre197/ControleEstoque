fb1d864a13900e40a606237497a7466c1f4b4517
Carlos Alexandre <alexandrec191@gmail.com>
2026-09-15 12:26:08 -0300

    Alterando o interface

--- interface.py
+++ interface.py
@@ -2,8 +2,12 @@ from PyQt6.QtWidgets import (
     QMainWindow,
     QWidget,
     QVBoxLayout,
-    QLabel
+    QHBoxLayout,
+    QPushButton,
+    QStackedWidget
 )
+
+from widgets.dashboard import DashboardWidget
 
 
 class Interface(QMainWindow):
@@ -12,17 +16,78 @@ class Interface(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Controle de Estoque")
+
         self.resize(1500, 900)
 
         self.criar_interface()
 
+    # ==========================================
+    # INTERFACE PRINCIPAL
+    # ==========================================
+
     def criar_interface(self):
 
         central = QWidget()
+
         self.setCentralWidget(central)
 
-        layout = QVBoxLayout(central)
+        layout_principal = QHBoxLayout(central)
 
-        titulo = QLabel("CONTROLE DE ESTOQUE")
+        # ==========================================
+        # MENU LATERAL
+        # ==========================================
 
-        layout.addWidget(titulo)
+        menu = QWidget()
+
+        menu.setFixedWidth(220)
+
+        layout_menu = QVBoxLayout(menu)
+
+        self.botao_inicio = QPushButton("Início")
+        self.botao_produtos = QPushButton("Produtos")
+        self.botao_estoque = QPushButton("Estoque")
+        self.botao_transferencia = QPushButton("Transferências")
+        self.botao_movimentacoes = QPushButton("Movimentações")
+
+        layout_menu.addWidget(self.botao_inicio)
+        layout_menu.addWidget(self.botao_produtos)
+        layout_menu.addWidget(self.botao_estoque)
+        layout_menu.addWidget(self.botao_transferencia)
+        layout_menu.addWidget(self.botao_movimentacoes)
+
+        layout_menu.addStretch()
+
+        # ==========================================
+        # ÁREA PRINCIPAL
+        # ==========================================
+
+        self.paginas = QStackedWidget()
+
+        self.dashboard = DashboardWidget()
+
+        self.paginas.addWidget(self.dashboard)
+
+        # ==========================================
+        # LAYOUT
+        # ==========================================
+
+        layout_principal.addWidget(menu)
+        layout_principal.addWidget(self.paginas)
+
+        # ==========================================
+        # EVENTOS
+        # ==========================================
+
+        self.botao_inicio.clicked.connect(
+            self.mostrar_inicio
+        )
+
+    # ==========================================
+    # MOSTRAR DASHBOARD
+    # ==========================================
+
+    def mostrar_inicio(self):
+
+        self.paginas.setCurrentWidget(
+            self.dashboard
+        )
